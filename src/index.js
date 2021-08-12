04179028ead50d50f6458c0dcac379032020befa
Mihail Dorozhko <dominick.alone@gmail.com>
2021-08-12 20:49:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((!array) || (array.length === 0)) {
+    return 0;
+  } else {
+    let min = 0;
+
+    array.forEach(function(item, index, array) {
+
+      if (item < min) {
+        min = item;
+      }
+    });
+
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((!array) || (array.length === 0)) {
+    return 0;
+  } else {
+    let max = 0;
+
+    array.forEach(function(item, index, array) {
+
+      if (item > max) {
+        max = item;
+      }
+    });
+
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((!array) || (array.length === 0)) {
+    return 0;
+  } else {
+    let avg = 0;
+
+    array.forEach(function(item, index, array) {
+      avg += item;
+    });
+
+    avg = avg / array.length;
+
+    return avg;
+  }
 }
